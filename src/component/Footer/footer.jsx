d307612198dd57bcc3fb7b4fa987ee36c194d748
Mohammad Saif <mohammadsaif0847@gmail.com>
2023-12-27 19:02:35 +0530

    Update footer.jsx

--- src/component/Footer/footer.jsx
+++ src/component/Footer/footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
   return (
     <div>
       <div className='attribution'>
-				Challenge by <a href='https://www.frontendmentor.io?ref=challenge'>Frontend Mentor</a>. Coded by <a href='#'>Mohammad Saif</a>.
+				Challenge by <a href='https://www.frontendmentor.io?ref=challenge'>Frontend Mentor</a>. Coded by <a href='https://www.linkedin.com/in/mohammad-saif-bca/' target = '_blank'>Mohammad Saif</a>.
 			</div>
     </div>
   )
